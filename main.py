7b3e6ba0df8b140ec1ee6193e08623460feba48f
Dmitry Rogov <dekoyre@gmail.com>
2017-12-18 10:24:55 +0300

    fix code

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import additional_functions as src
 from os import listdir
 
-train_sets = ['digits', 'Avenir', 'Courier', 'Marion']
+train_sets = ['icons', 'digits', 'Avenir', 'Courier', 'Marion']
 path_to_train_samples = 'assets/images/train/'
 path_to_output_vectors = 'output/vectors/'
 
@@ -23,7 +23,10 @@ range_param = 15
 target_is_darker = True
 
 # is target image is darker than background?
-max_iterations = 15
+max_iterations = 20
+
+# is init
+is_init = False
 
 # [===>--<==>--<==>--<==>--<==>     end of settings     <==>--<==>--<==>--<==>--<===]
 #
@@ -65,7 +68,8 @@ def initialize_vectors():
 
 img = src.cv2.imread(path_to_train_samples + train_samples_list[0], src.cv2.IMREAD_COLOR)
 image_shape = img.shape
-# initialize_vectors()
+if is_init:
+    initialize_vectors()
 
 vector_list = listdir(path_to_output_vectors)
 
@@ -77,7 +81,7 @@ n_vectors = len(vector_list)
 def hamming_distance(a, b):
     c = 0
     for i in range(n_elements):
-        c += a[0, i] * b[0, i]
+        c += a[i, 0] * b[i, 0]
     return (n_elements - c) / 2
 
 
@@ -89,27 +93,44 @@ def train_network():
     for i in range(0, n_vectors):
         print('Begin stage ' + str(i + 1) + '/' + str(n_vectors) + '')
         x = np.load(path_to_output_vectors + vector_list[i])
-        weights_matrix = weights_matrix + np.outer(x, x)
+        weights_matrix += np.outer(x, x)
         print('Stage complete ' + str(i + 1) + '/' + str(n_vectors) + '\n')
 
-    weights_matrix = weights_matrix / n_vectors
+    # print(type(weights_matrix))
+    # print(weights_matrix)
+    # print(weights_matrix / 4)
 
     for i in range(0, n_elements):
         weights_matrix[i, i] = 0
 
-    print(weights_matrix)
-    print(weights_matrix.shape)
+    arrays = []
+    for d in range(n_vectors):
+        arrays.append(np.load(path_to_output_vectors + vector_list[d]))
+
+    for i in range(n_elements):
+        for j in range(n_elements):
+            for d in range(n_vectors):
+                weights_matrix[i, j] += arrays[d][0, i] * arrays[d][0, j]
+
+    weights_matrix = np.matrix(weights_matrix / n_elements)
 
     np.save('output/matrix/matrix', weights_matrix)
     print('TRAINING COMPLETE\n\n\n')
 
 
-train_network()
-W = np.load('output/matrix/matrix.npy')
+if is_init:
+    train_network()
+
+W = np.matrix(np.load('output/matrix/matrix.npy'))
 np.savetxt('output/matrix/matrix.txt', W, delimiter=', ', fmt='%s')
 print ('Weights matrix has loaded')
 
-v = src.image_to_array('input/7.bmp',
+# x = src.image_to_array('input/7.bmp')
+# # print (hamming_distance(x, (W * x.T).T))
+# print(np.equal(x, (W * x.T).T))
+# # print(((W * x.T).T).shape)
+
+v = src.image_to_array('input/1.bmp',
                        background_color,
                        range_param,
                        target_is_darker)
@@ -126,17 +147,17 @@ k = 0
 
 while True:
     k += 1
-    y = v.copy()
+    y = v.T.copy()
     y_next = W * y.copy()
-    for i in range(y.shape[1]):
-        y_next[0, i] = threshold(y_next[0, i])
+    for i in range(n_elements):
+        y_next[i, 0] = threshold(y_next[i, 0])
     distance = hamming_distance(y, y_next)
-    print(distance)
     is_result_found = distance == 0
     if k == max_iterations | is_result_found:
         break
 
 if is_result_found:
-    src.cv2.imshow('123', np.reshape(y, (image_shape[0], image_shape[1])))
+    print ('result')
+    # src.cv2.imshow('123', np.reshape(y, (image_shape[0], image_shape[1])))
 else:
     print('no result')
